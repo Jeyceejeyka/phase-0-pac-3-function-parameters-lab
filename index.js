18f627d416326679b446ed5f4db4cb37d89401ab
jeycee <jeyceejeyka635@gmail.com>
2024-11-23 02:46:26 +0300

    Adds parameters that are either overidden/optional/and others

---
+++ index.js
@@ -0,0 +1,52 @@
+/*function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction("Aki");
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}`;
+}
+introductionWithLanguage("Aki", "Ember.js");
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in`
+}
+
+introductionWithLanguageOptional("Gracie");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    //let language = "python";
+    return `Hi, my name is ${name} and I am learning to program in ${language}`;
+}
+
+introductionWithLanguageOptional("Gracie","python")
+*/
+
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction("Aki"); 
+
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Aki", "Ember.js"); 
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional("Gracie"); 
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
+console.log(introductionWithLanguageOptional("Gracie", "Python"));
+
+
+
