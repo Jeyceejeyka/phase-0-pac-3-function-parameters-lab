3aa7c46e24e8f97f1d01f32bc8c57640032da095
jeycee <jeyceejeyka635@gmail.com>
2024-11-23 20:12:26 +0300

    Adds bail and removed comments

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-/*function introduction(name) {
-    return `Hi, my name is ${name}.`;
-}
-introduction("Aki");
-
-function introductionWithLanguage(name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}`;
-}
-introductionWithLanguage("Aki", "Ember.js");
-
-
-function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in`
-}
-
-introductionWithLanguageOptional("Gracie");
-
-function introductionWithLanguageOptional(name, language = "JavaScript") {
-    //let language = "python";
-    return `Hi, my name is ${name} and I am learning to program in ${language}`;
-}
-
-introductionWithLanguageOptional("Gracie","python")
-*/
 
 
 function introduction(name) {
